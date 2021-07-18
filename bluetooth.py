f11520bf274a6a7525283c5bd77c9a3d4dfb1717
Awais Hussain <awais.hussain@covvi.com>
2021-07-18 22:24:59 +0100

    Linked bluetooth commands and ar detection

--- bluetooth.py
+++ bluetooth.py
@@ -7,58 +7,65 @@ system = platform.system()
 address = "5BC1B0BC-5A3F-4560-ACF0-9D1C4DD826A8" if system == "Darwin" else "04:91:62:A1:47:12"
 READ_UUID = "C47C4423-F712-491B-85E6-E989A053B1B1"
 WRITE_UUID = "C47C4423-F712-491B-85E6-E989A053B1B2"
+client = BleakClient(address)
 
-val = "1"
 
 def read_data(sender: int, data: bytearray):
     print(f"{sender}: {data}")
 
-async def send(command, client):
-    print(type(command))
+async def send(command):
+    print(type(command), command)
     await client.write_gatt_char(WRITE_UUID, command)
     time.sleep(0.5)
     print("Sent Data", command)
 
-async def stfu(client):
-    await send(b'TM03040064', client)
+def openHand():
+    global nextCommand
+    nextCommand = b'TM031F0064'
 
-async def openHand(client):
-    await send(b'TM031F0064', client)
+def closeHand():
+    global nextCommand
+    nextCommand = b'TM031FFF64'
 
-async def closeHand(client):
-    await send(b'TM031FFF64', client)
+async def enterTestMode():
+    await send(b'TT0101')
 
-async def enterTestMode(client):
-    await send(b'TT0101', client)
-
-async def exitTestMode(client):
-    await send(b'TT0100', client)
+async def exitTestMode():
+    await send(b'TT0100')
 
 
 async def run():
-    client = BleakClient(address)
+    
     try:
         await client.connect()
         time.sleep(0.5)
         await client.start_notify(READ_UUID, read_data)
         time.sleep(0.5)
-        await enterTestMode(client)
-        # await openHand(client)
-        # await closeHand(client)
-        # time.sleep(1.5)
+        await enterTestMode()
         end = False
 
-        while(not end):
-            res = input("What would you like to do?")
-            if (res == "o"):
-                await openHand(client)
-            elif (res == "c"):
-                await closeHand(client)
-            elif (res == "stfu"):
-                await stfu(client)
+        global nextCommand
+        nextCommand = ""
+        while(True):
+            print(nextCommand)
+            if (nextCommand == b'TM031FFF64'):
+                await send(nextCommand)
+                time.sleep(3.0)
+                openHand()
+            elif (nextCommand == b'TM031F0064'):
+                await send(nextCommand)
+                time.sleep(0.2)
+                nextCommand = ""
             else:
-                await exitTestMode(client)
-                end = True
+                time.sleep(0.2)
+            # res = input("What would you like to do?")
+            # if (res == "o"):
+            #     await openHand()
+            # elif (res == "c"):
+            #     await closeHand()
+            # else:
+            #     await exitTestMode()
+            #     end = True
     except Exception as e:
         print("error")
         print(e)
@@ -70,21 +77,5 @@ async def run():
 def start(loop):
     loop.run_until_complete(run())
 
-print(val)
-
 
 # 5BC1B0BC-5A3F-4560-ACF0-9D1C4DD826A8
-
-# import asyncio
-# from bleak import BleakClient
-
-# address = "5BC1B0BC-5A3F-4560-ACF0-9D1C4DD826A8"
-# MODEL_NBR_UUID = "00002a24-0000-1000-8000-00805f9b34fb"
-
-# async def run(address):
-#     async with BleakClient(address) as client:
-#         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-#         print("Model Number: {0}".format("".join(map(chr, model_number))))
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(run(address))
--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from bluetooth import *
 
 import threading
 
+val = "2"
+
 loop = asyncio.get_event_loop()
 bluetoothThread = threading.Thread(target=start, args=(loop, ))
 bluetoothThread.start()
