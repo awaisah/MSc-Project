0ea1e34e44caa2772dec68d0cfca7cf9895d16c8
Awais Hussain <awais.hussain@covvi.com>
2021-07-18 19:13:41 +0100

    Scripts Working Independantly

--- detect_aruco_video.py
+++ detect_aruco_video.py
@@ -5,6 +5,61 @@ import imutils
 import time
 import cv2
 import sys
+import math
+import time
+from AppKit import NSBeep
+
+def say(msg = "Finish", voice = "Victoria"):
+    os.system(f'say -v {voice} {msg}')
+
+def rotatePoint(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
+
+def rotateCorners(corners):
+	(topLeft, topRight, bottomRight, bottomLeft) = corners
+
+	print(topLeft)
+	print(topRight)
+	print(bottomRight)
+	print(bottomLeft)
+
+	# Set the top left corner to be (0,0) and move other points to be relative to this
+	offsetX = topLeft[0]
+	offsetY = topLeft[1]
+
+	topLeft[0] = 0
+	topLeft[1] = 0
+
+	topRight[0] = topRight[0] - offsetX
+	topRight[1] = topRight[1] - offsetY
+
+	bottomRight[0] = bottomRight[0] - offsetX
+	bottomRight[1] = bottomRight[1] - offsetY
+
+	bottomLeft[0] = bottomLeft[0] - offsetX
+	bottomLeft[1] = bottomLeft[1] - offsetY
+
+	diagonals = []
+	diagonals.append(math.sqrt((bottomLeft[0] - topRight[0]) ** 2 + (bottomLeft[1] - topRight[1]) ** 2))
+	diagonals.append(math.sqrt((topLeft[0] - bottomRight[0]) ** 2 + (topLeft[1] - bottomRight[1]) ** 2))
+
+	sides = []
+	sides.append(math.sqrt((topLeft[0] - topRight[0]) ** 2 + (topLeft[1] - topRight[1]) ** 2)) # top
+	sides.append(math.sqrt((topRight[0] - bottomRight[0]) ** 2 + (topRight[1] - bottomRight[1]) ** 2)) # right
+	sides.append(math.sqrt((bottomRight[0] - bottomLeft[0]) ** 2 + (bottomRight[1] - bottomLeft[1]) ** 2)) # bottom
+	sides.append(math.sqrt((bottomLeft[0] - topLeft[0]) ** 2 + (bottomLeft[1] - topLeft[1]) ** 2)) # left
+
+	return (topLeft, topRight, bottomRight, bottomLeft, sides) 
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -12,6 +67,7 @@ ap.add_argument("-t", "--type", type=str,
 	default="DICT_ARUCO_ORIGINAL",
 	help="type of ArUCo tag to detect")
 args = vars(ap.parse_args())
+
 
 # define names of each possible ArUco tag OpenCV supports
 ARUCO_DICT = {
@@ -63,28 +119,40 @@ while True:
 	(corners, ids, rejected) = cv2.aruco.detectMarkers(frame,
 		arucoDict, parameters=arucoParams)
 
-    # verify *at least* one ArUco marker was detected
+	# verify *at least* one ArUco marker was detected
 	if len(corners) > 0:
 		# flatten the ArUco IDs list
 		ids = ids.flatten()
+
+		averageLength = 100
+		lastBeep = -1
+
 		# loop over the detected ArUCo corners
 		for (markerCorner, markerID) in zip(corners, ids):
 			# extract the marker corners (which are always returned
 			# in top-left, top-right, bottom-right, and bottom-left
 			# order)
 			corners = markerCorner.reshape((4, 2))
-			(topLeft, topRight, bottomRight, bottomLeft) = corners
+			# (topLeft, topRight, bottomRight, bottomLeft) = corners
+			(topLeft, topRight, bottomRight, bottomLeft, sides) = rotateCorners(corners)
 			# convert each of the (x, y)-coordinate pairs to integers
 			topRight = (int(topRight[0]), int(topRight[1]))
 			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
 			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
 			topLeft = (int(topLeft[0]), int(topLeft[1]))
 
+			averageLength = int((sides[0] + sides[1] + sides[2] + sides[3]) / 4)
+
 			# draw the bounding box of the ArUCo detection
 			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
 			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
 			cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
 			cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+
+			cv2.line(frame, [0,0], [averageLength, 0], (0, 255, 0), 2)
+			cv2.line(frame, [averageLength, 0], [averageLength, averageLength], (0, 255, 0), 2)
+			cv2.line(frame, [averageLength, averageLength], [0, averageLength], (0, 255, 0), 2)
+			cv2.line(frame, [0, averageLength], [0,0], (0, 255, 0), 2)
 			# compute and draw the center (x, y)-coordinates of the
 			# ArUco marker
 			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
@@ -95,12 +163,25 @@ while True:
 				(topLeft[0], topLeft[1] - 15),
 				cv2.FONT_HERSHEY_SIMPLEX,
 				0.5, (0, 255, 0), 2)
+			cv2.putText(frame, str(averageLength),
+				[10,20],
+				cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (0, 0, 255), 2)
 	# show the output frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
+	if (len(corners) > 0 and averageLength >= 160):
+		# sys.stdout.write('\a')
+		# sys.stdout.flush()
+		print(lastBeep)
+		if (time.perf_counter() - lastBeep > 1 or lastBeep == -1):
+			lastBeep = time.perf_counter() * 1
+			NSBeep()
+	
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+
